c65bdc173941a0d51e29bdbb5b1b4803807a93f1
noah <noah@apartalk.com>
2021-04-26 18:43:03 +0900

    factoryboy 적용한 예제 테스트 코드 작성

--- tests/core/domains/user/repository/test_user_repository.py
+++ tests/core/domains/user/repository/test_user_repository.py
@@ -22,8 +22,17 @@ def test_get_user(session):
     assert user_entity == user.to_entity()
 
 
-def test_create_user_when_use_factory_boy(session, create_users):
+def test_create_user_when_use_create_users_fixture_then_make_two_users(session, create_users):
     users = session.query(UserModel).all()
 
     assert len(users) == 2
     assert users[0].nickname == "test_user_0"
+
+
+def test_compare_create_user_when_use_build_batch_and_create_users_fixture(session, create_users, normal_user_factory):
+    fixture_users = session.query(UserModel).all()
+    build_batch_users = normal_user_factory.build_batch(size=3, nickname="Noah", sex="F")
+
+    assert len(fixture_users) == 2
+    assert len(build_batch_users) == 3
+    assert fixture_users[0].sex != build_batch_users[0].sex == 'F'
